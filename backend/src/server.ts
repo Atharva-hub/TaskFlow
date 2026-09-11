140a784a0bdba3dba89c025cf9fbc3883ee6f62b
Atharva-hub <avedpathak60@gmail.com>
2026-09-11 20:20:32 +0530

    chore: set up TypeScript + Express project with ESM config

---
+++ backend/src/server.ts
@@ -0,0 +1,17 @@
+import express,{Request,Response} from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/health',(req:Request,res:Response)=>{
+    res.json({status:"Server is Running"});
+});
+
+app.get('/ping',(req:Request,res:Response)=>{
+    res.json({message:"ok"});
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+});
